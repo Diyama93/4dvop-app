b04f637fb3c87ebf870b8e98d565b0a6291eac19
Diyama93 <diyamabienvenue@gmail.com>
2020-04-28 19:30:16 +0200

    script modification

--- 03_jenkins/4dvop-app.sh
+++ 03_jenkins/4dvop-app.sh
@@ -1,16 +1,15 @@
 #!bin/bash
-sudo yum install ntp -y
-sudo systemctl start ntpd
+yum install ntp -y
+systemctl start ntpd
 systemctl enable ntpd
-sudo systemctl enable ntpd
+systemctl enable ntpd
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-sudo usermod -aG docker centos
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo yum -y install bash-completion
-sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+systemctl enable docker
+systemctl start docker
+yum -y install bash-completion
+curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 cd 4dvop-app/02_docker 
 docker-compose up
