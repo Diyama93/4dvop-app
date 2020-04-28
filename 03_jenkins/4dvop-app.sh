062ff2deaa330110bb3f2da33e3e14ab90a3668e
Diyama93 <diyamabienvenue@gmail.com>
2020-04-28 19:16:38 +0200

    script

--- 03_jenkins/4dvop-app.sh
+++ 03_jenkins/4dvop-app.sh
@@ -1,4 +1,16 @@
-sudo yum -y install git
-git clone git@github.com:Diyama93/4dvop-app.git
-. 4dvop-app/03_jenkins/4dvop-app.sh
-rm -Rf 4dvop-app
+#!bin/bash
+sudo yum install ntp -y
+sudo systemctl start ntpd
+systemctl enable ntpd
+sudo systemctl enable ntpd
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker centos
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo yum -y install bash-completion
+sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+cd 4dvop-app/02_docker 
+docker-compose up
